02170fe08d7e898b4e36dda0fe858e2392a3e989
fukura <jxay_4@yahoo.co.jp>
2018-10-21 17:30:01 +0900

    バナー広告用プラグイン追加

---
+++ minigameUI/Assets/Scripts/Scenes/InitializeScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitializeScene : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+		var adMob = GameObject.Find("AdMob");
+		DontDestroyOnLoad(adMob);
+		SystemManager.GetInstance().LoadScene("TitleScene");
+	}
+}
+	
