bc3fa6eb3996881d0fec1ebc0138cbefb62ed47a
fukura <jxay_4@yahoo.co.jp>
2019-03-24 10:33:59 +0900

    アプリIDと広告ユニットIDをサンプルではなくうちのプロジェクト用に変更

--- minigameUI/Assets/Scripts/AdMob.cs
+++ minigameUI/Assets/Scripts/AdMob.cs
@@ -12,8 +12,8 @@ public class AdMob : MonoBehaviour
 	void Start()
 	{
 
-		// アプリID、 これはテスト用
-		string appId = "ca-app-pub-3940256099942544~3347511713";
+		// アプリID
+		string appId = "ca-app-pub-4760513462703258~5956190373";
 
 		// Initialize the Google Mobile Ads SDK.
 		MobileAds.Initialize(appId);
@@ -25,7 +25,7 @@ public class AdMob : MonoBehaviour
 	{
 
 		// 広告ユニットID これはテスト用
-		string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+		string adUnitId = "ca-app-pub-4760513462703258/1130883886";
 
 		// Create a 320x50 banner at the top of the screen.
 		//BannerView bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
